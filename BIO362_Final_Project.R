a222f2511d5c6745dca4fc5f1b71e0e9ae4e0e04
jfleshel <jfleshel@fandm.edu>
2024-10-31 15:59:42 -0400

    Update BIO362_Final_Project.R

--- BIO362_Final_Project.R
+++ BIO362_Final_Project.R
@@ -1,8 +1,4 @@
 #DSEE Final Project
 
-
-library(readr)
-        
-#Silvia
-SmartLocationDatabase <- read_csv("G:/My Drive/BIO - Final Proyect/EPA_SmartLocationDatabase_V3_Jan_2021_Final.csv")
-EJSCREEN <- read_csv("G:/My Drive/BIO - Final Proyect/EJSCREEN.csv")
+EJScreen <- read.csv("/Users/jamiefleshel/Desktop/F&M/Fall_2024/DSEE/Intro_to_R/Final_Project/EJSCREEN.csv")
+SmartLocation <- read.csv("/Users/jamiefleshel/Desktop/F&M/Fall_2024/DSEE/Intro_to_R/Final_Project/SmartLocationDatabase.csv")

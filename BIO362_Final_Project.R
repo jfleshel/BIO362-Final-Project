6676b5fac129ce92ec627a86b381865a1c2d7e2e
jfleshel <jfleshel@fandm.edu>
2024-11-07 14:02:07 -0500

    Update BIO362_Final_Project.R

--- BIO362_Final_Project.R
+++ BIO362_Final_Project.R
@@ -4,10 +4,11 @@ library(readr)
 
 #J
 EJSCREEN <- read.csv("/Users/jamiefleshel/Desktop/F&M/Fall_2024/DSEE/Intro_to_R/Final_Project/EJSCREEN.csv")
-SmartLocationDatabase <- read.csv("/Users/jamiefleshel/Desktop/F&M/Fall_2024/DSEE/Intro_to_R/Final_Project/SmartLocationDatabase.csv")
 
 #S
-SmartLocationDatabase <- read_csv("G:/My Drive/BIO - Final Proyect/EPA_SmartLocationDatabase_V3_Jan_2021_Final.csv")
 EJSCREEN <- read_csv("G:/My Drive/BIO - Final Proyect/EJSCREEN.csv")
 
 library(dplyr)
+
+PennsylvaniaData <- filter(EJSCREEN, ST_ABBREV == "PA")
+
